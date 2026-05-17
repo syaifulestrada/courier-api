9874abcea82ed007f00e77b36f12266c0f54ab72
syaifulestrada <syaifultheminister@gmail.com>
2026-05-17 20:27:34 +0700

    Create StoreCourierRequest validation

---
+++ app/Http/Requests/StoreCourierRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCourierRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:couriers,email',
+            'phone' => 'required|string|max:13',
+            'level' => 'required|integer|between:1,5',
+            'address' => 'required|string',
+            'is_active' => 'required|boolean',
+            'registered_at' => 'required|date|before_or_equal:today',
+        ];
+    }
+}
