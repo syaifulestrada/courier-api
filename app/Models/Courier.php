ce449f8d4bde32b5b03fbe5b95969e1ebe44d8a8
syaifulestrada <syaifultheminister@gmail.com>
2026-05-17 20:28:27 +0700

    Add registered_at column to fillable in Courier model

--- app/Models/Courier.php
+++ app/Models/Courier.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[Fillable(['name', 'email', 'phone', 'level',  'address', 'is_active'])]
+#[Fillable(['name', 'email', 'phone', 'level',  'address', 'is_active', 'registered_at'])]
 class Courier extends Model
 {
     /** @use HasFactory<CourierFactory> */
